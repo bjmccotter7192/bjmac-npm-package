c62379301529e6d1b615197c7c229214efdd0132
BJ McCotter <bjmccotter7192@gmail.com>
2021-11-22 23:13:47 -0600

    perf(Button-Ticket): Major push with new button

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
+import {Button} from './button';
 
 // Delete me
 export const Thing = () => {
-  return <div className="custom-text">the snozzberries taste like snozzberries BJ CHANGING FOR TESTING</div>;
+  return <div className="custom-text">
+    <Button/>
+    the snozzberries taste like snozzberries BJ CHANGING FOR TESTING
+  </div>;
 };
