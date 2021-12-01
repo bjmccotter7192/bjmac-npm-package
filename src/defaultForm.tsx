1d77d4daecc80fabf5b1ac21b227c3673ba7feb1
BJ McCotter <bj.mccotter@adhocteam.us>
2021-12-01 15:53:02 -0600

    fix: setting up local development

--- src/defaultForm.tsx
+++ src/defaultForm.tsx
@@ -3,56 +3,73 @@ import { Formik, Field, Form } from 'formik';
 import { TextField } from '@department-of-veterans-affairs/formulate';
 
 export const DefaultForm = () => {
-  return (
-    <div>
-      <h1>Sign Up</h1>
-      <Formik
-        initialValues={{
-          firstName: '',
-          lastName: '',
-          testVal: '',
-          email: '',
-        }}
-        onSubmit={async values => {
-          await new Promise(r => setTimeout(r, 500));
-          alert(JSON.stringify(values, null, 2));
-        }}
-      >
-        <Form>
-          <label htmlFor="firstName">First Name</label>
-          <Field 
-            id="firstName" 
-            name="firstName" 
-            placeholder="Jane"
-            required
-        />
-
-          <label htmlFor="lastName">Last Name</label>
-          <Field 
-            id="lastName" 
-            name="lastName" 
-            placeholder="Doe"
-            required
-        />
-
-          <label htmlFor="testVal">Testing TextField</label>
-          <TextField
-            name="Testing Field Data"
-            label="This is the label for the Field Data"
-            required
-          />
-
-          <label htmlFor="email">Email</label>
-          <Field
-            id="email"
-            name="email"
-            placeholder="jane@acme.com"
-            type="email"
-            required
-          />
-          <button type="submit">Submit</button>
-        </Form>
-      </Formik>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Sign Up</h1>
+            <Formik
+                initialValues={{
+                    firstName: '',
+                    lastName: '',
+                    testVal: '',
+                    email: '',
+                }}
+                onSubmit={async values => {
+                    await new Promise(r => setTimeout(r, 500));
+                    alert(JSON.stringify(values, null, 2));
+                }}
+            >
+                <Form>
+                    <div>
+                        <label htmlFor="firstName">First Name</label>
+                        &nbsp;
+                        <Field 
+                            id="firstName" 
+                            name="firstName" 
+                            placeholder="Jane"
+                            required
+                        />
+                    </div>
+                    <br/>
+                    <div>
+                        <label htmlFor="lastName">Last Name</label>
+                        <Field 
+                            id="lastName" 
+                            name="lastName" 
+                            placeholder="Doe"
+                            required
+                        />
+                    </div>
+                    <br/>
+                    <div>
+                        <label htmlFor="testVal">Testing TextField</label>
+                        <TextField
+                            id="testVal1"
+                            name="Testing Field Data"
+                            label="This is the label for the Field Data"
+                            required
+                        />
+                        <Field
+                            id="testVal"
+                            name="testVal"
+                            placeholder="test val"
+                            required
+                        />
+                    </div>
+                    <br/>
+                    <div>
+                        <label htmlFor="email">Email</label>
+                        <Field
+                            id="email"
+                            name="email"
+                            placeholder="jane@acme.com"
+                            type="email"
+                            required
+                        />
+                    </div>
+                    <br/>
+                    <button type="submit">Submit</button>
+                </Form>
+            </Formik>
+        </div>
+    );
 };
