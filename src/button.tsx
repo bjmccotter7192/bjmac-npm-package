baee83ebe0d4257b98a8b450c929f798f34a0716
BJ McCotter <bj.mccotter@adhocteam.us>
2022-07-28 22:27:48 -0500

    perf: installing eslint and eslintrc.js

--- src/button.tsx
+++ src/button.tsx
@@ -1,5 +1,15 @@
 import * as React from 'react';
 
 export const Button = () => {
-    return <button>Push Test Number 3!</button>
+  return (
+    <>
+      <h1>Here Is A Button</h1>
+
+      <h2>Push Button Below V V V</h2>
+
+      <h3>
+          <button>Push Test Number 3!</button>
+      </h3>
+    </>
+  )
 } 
